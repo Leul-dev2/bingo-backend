514a706bd52265618e4cf2f96363934598dd039f
alazar <alazarfikadu15@gmail.com>
2025-05-19 17:24:58 +0300

    update gameroute for consistency in the complete to reset game

--- routes/gameRoutes.js
+++ routes/gameRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require("../models/user");
 const Game = require("../models/game");
-const resetGame = req.app.get("resetGame");
+
 
 
 
