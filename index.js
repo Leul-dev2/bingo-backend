0f19f5541f34165fdd8ca503314441510736c2ab
alazar <alazarfikadu15@gmail.com>
2025-06-09 13:23:14 +0300

    update the gamid to numPlayers

--- index.js
+++ index.js
@@ -92,6 +92,7 @@ function resetGame(gameId) {
 
 
 
+
 function emitPlayerCount(gameId) {
   const playerCount = gameRooms[gameId]?.length || 0;
   io.to(gameId).emit("playerCountUpdate", { gameId, playerCount });
@@ -125,22 +126,7 @@ function emitPlayerCount(gameId) {
 
         // ✅ Send player count to all in room
         const numberOfPlayers = gameSessions[gameId].length;
-        io.to(gameId).emit("gameid", { gameId, numberOfPlayers });
-
-        // // Initialize gameRooms if it doesn't exist
-        // if (!gameRooms[gameId]) {
-        //     gameRooms[gameId] = [];
-        // }
-
-        // // Add the player to the gameRooms
-        // if (!gameRooms[gameId].includes(telegramId)) {
-        //     gameRooms[gameId].push(telegramId);
-        // }
-
-        // // Emit the updated player count
-        // io.to(gameId).emit("playerCountUpdate", { gameId, playerCount: gameRooms[gameId].length });
-
-
+        io.to(gameId).emit("numPlayers", { gameId, numberOfPlayers });
     });
 
 
@@ -188,8 +174,13 @@ function emitPlayerCount(gameId) {
     });
 
 
-   
+    socket.on("getPlayerCount", ({ gameId }) => {
+        socket.join(gameId);  // 👈 Join the room
+        const playerCount = gameRooms[gameId]?.length || 0;
+        socket.emit("playerCountUpdate", { gameId, playerCount });
+    });
 
+    
     socket.on("joinGame", ({ gameId, telegramId }) => {
         socket.join(gameId);
 
@@ -199,11 +190,11 @@ function emitPlayerCount(gameId) {
         // You can store socket.telegramId = telegramId if needed
     });
 
-    socket.on("getPlayerCount", ({ gameId }) => {
-        socket.join(gameId);  // 👈 Join the room
-        const playerCount = gameRooms[gameId]?.length || 0;
-        socket.emit("playerCountUpdate", { gameId, playerCount });
-    });
+
+
+    
+
+    ///the second page for game playing the game drawing page
 
     socket.on("gameCount", ({ gameId }) => {
     if (!gameDraws[gameId]) {
