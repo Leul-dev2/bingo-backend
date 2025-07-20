23d335726e88d55e8b3144b0a93c2bcf701623ee
alazar <alazarfikadu15@gmail.com>
2025-07-20 14:48:29 +0300

    comment out the delete redis code inside gameroute

--- routes/gameRoutes.js
+++ routes/gameRoutes.js
@@ -34,11 +34,11 @@ router.post("/start", async (req, res) => {
 
     
 
-    const keys = await redis.keys("game*");
-    if (keys.length > 0) {
-      await redis.del(...keys);
-      console.log("✅ Cleared all game-related Redis keys.");
-    }
+    // const keys = await redis.keys("game*");
+    // if (keys.length > 0) {
+    //   await redis.del(...keys);
+    //   console.log("✅ Cleared all game-related Redis keys.");
+    // }
 
 
     // Proceed with join: lock user, deduct balance
