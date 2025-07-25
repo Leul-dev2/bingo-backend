d6d2234f4d841c8b0314109032788635a22db5b5
alazar <alazarfikadu15@gmail.com>
2025-07-25 22:16:25 +0300

    update the userJoined and disconnect to handle user connection in 4 second

--- sockets/gameSocket.js
+++ sockets/gameSocket.js
@@ -21,6 +21,7 @@ const { // <-- Add this line
 } = require("../utils/redisKeys"); // <-- Make sure the path is correct
 const pendingDisconnectTimeouts = new Map(); // Key: `${telegramId}:${gameId}`, Value: setTimeout ID
 const ACTIVE_DISCONNECT_GRACE_PERIOD_MS = 4 * 1000;
+const ACTIVE_SOCKET_TTL_SECONDS = 60 * 3; 
 
 
 
