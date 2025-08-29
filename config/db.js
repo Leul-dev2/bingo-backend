86b820f19c273ea0749ec1e978a55d5cf9c26026
alazar <alazarfikadu15@gmail.com>
2025-08-29 15:49:29 +0300

    console mongo url

--- config/db.js
+++ config/db.js
@@ -1,18 +1,10 @@
 console.log("Mongo URI loaded from environment inside dbjs:", process.env.MONGODB_URI);
 
 const mongoose = require("mongoose");
+const mongoUri = process.env.MONGODB_URI;
+mongoose.connect(mongoUri, { /* options */ })
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.error("MongoDB connection error:", err));
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("✅ MongoDB Connected");
-  } catch (error) {
-    console.error("❌ MongoDB Connection Error:", error.message);
-    process.exit(1);
-  }
-};
 
 module.exports = connectDB;
