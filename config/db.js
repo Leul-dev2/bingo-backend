75eef75c8ff8279c5469102a2e5452e2d0ae042e
alazar <alazarfikadu15@gmail.com>
2025-08-29 15:51:16 +0300

    console mongo url

--- config/db.js
+++ config/db.js
@@ -1,10 +1,21 @@
-console.log("Mongo URI loaded from environment inside dbjs:", process.env.MONGODB_URI);
-
 const mongoose = require("mongoose");
-const mongoUri = process.env.MONGODB_URI;
-mongoose.connect(mongoUri, { /* options */ })
-    .then(() => console.log("MongoDB connected"))
-    .catch(err => console.error("MongoDB connection error:", err));
 
+// Use the exact environment variable you set in CapRover
+const mongoUri = process.env.MONGODB_URI;
+
+console.log("Mongo URI loaded from environment inside dbjs:", mongoUri);
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(mongoUri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("MongoDB connected");
+    } catch (err) {
+        console.error("MongoDB connection error:", err);
+        process.exit(1); // stop the app if DB connection fails
+    }
+};
 
 module.exports = connectDB;
