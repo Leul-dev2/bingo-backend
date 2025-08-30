31431651eedd47ffaf85035b4351f9ed3ade7d06
alazar <alazarfikadu15@gmail.com>
2025-08-30 20:55:22 +0300

    update the second of hrace time

--- sockets/gameSocket.js
+++ sockets/gameSocket.js
@@ -26,7 +26,7 @@ const { // <-- Add this line
 const { Socket } = require("socket.io");
 const pendingDisconnectTimeouts = new Map(); // Key: `${telegramId}:${gameId}`, Value: setTimeout ID
 const ACTIVE_DISCONNECT_GRACE_PERIOD_MS = 2 * 1000; // For card selection lobby (10 seconds)
-const JOIN_GAME_GRACE_PERIOD_MS = 2 * 1000; // For initial join/live game phase (5 seconds)
+const JOIN_GAME_GRACE_PERIOD_MS = 6 * 1000; // For initial join/live game phase (5 seconds)
 const ACTIVE_SOCKET_TTL_SECONDS = 60 * 3;
 
 
