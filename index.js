2e3ad2bfc2d15067dda6fdbe59d8c4e5f0e08bb5
alazar <alazarfikadu15@gmail.com>
2025-08-31 18:24:19 +0300

    aDd socket js

--- index.js
+++ index.js
@@ -30,10 +30,10 @@ const GameHistory = require('./models/GameHistory');
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors: { origin: "https://frontend.bingoogame.com", methods: ["GET", "POST"] },
+    cors: { origin: "*", methods: ["GET", "POST"] },
     // --- ADD THESE LINES TO CONFIGURE PING-PONG ---
-    pingInterval: 2000, // Server sends a ping every 5 seconds
-    pingTimeout: 2000,  // Server waits 3 seconds for a pong response
+    pingInterval: 5000, // Server sends a ping every 5 seconds
+    pingTimeout: 5000,  // Server waits 3 seconds for a pong response
     // ------------------------------------------------
 });
 
