ce76c1b949b896d421556c4329e12f622e43eabd
alazar <alazarfikadu15@gmail.com>
2025-11-11 18:06:23 +0300

    change origin connection for all

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const GameHistory = require('./models/GameHistory');
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors: { origin: "https://bingo21.netlify.app", methods: ["GET", "POST"] },
+    cors: { origin: "*", methods: ["GET", "POST"] },
     // --- ADD THESE LINES TO CONFIGURE PING-PONG ---
    pingInterval: 15000,
    pingTimeout: 25000,
